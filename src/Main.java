840b36c9da12c7b37c1255a9b996407f3bfb4432
Maxence Adnot <adnot.maxence@gmail.com>
2013-10-02 11:38:28 +0200

    ToXML

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,42 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+import universite.administratif.Laboratoire;
 import universite.administratif.Universite;
 import universite.batiment.SalleTP;
+import universite.personnel.ChargeRecherche;
 
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		
+		
 		Universite universite = new Universite();
+		
+		Laboratoire l1 = new Laboratoire();
+		Laboratoire l2 = new Laboratoire();
+		
+		universite.ajoutLaboratoire(l1);
+		universite.ajoutLaboratoire(l2);
+		
+		
+		ChargeRecherche c1 = new ChargeRecherche(0, "lel");
+		
+		l1.ajouteChercheur(c1);
+		
+		ChargeRecherche c2 = ((ChargeRecherche)c1.clone());
+		c2.setAnciennete(10);
+		l2.ajouteChercheur(c2);
+		
 		
 		try {
 			XMLManager.exportToXML(universite);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
